0e8326c948b7f1d6066c80b78a13bf6363c197d7
Frederik12345678 <frederiknagel9@gmail.com>
2024-10-31 12:10:44 +0100

    tilføjet lidt kode

--- Object detection/utils.py
+++ Object detection/utils.py
@@ -6,6 +6,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+#Reads xml files
 def parse_xml(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
@@ -25,7 +26,8 @@ def parse_xml(xml_file):
 
     return pothole_bboxes
 
-def calculate_iou(boxA, boxB):
+# computes iou of two bounding boxes
+def iou(boxA, boxB):
     # Calculate the (x, y) coordinates of the intersection rectangle
     xA = max(boxA[0], boxB[0])
     yA = max(boxA[1], boxB[1])
@@ -45,6 +47,7 @@ def calculate_iou(boxA, boxB):
 
     return iou
 
+#?
 def selective_search(image):
     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
     ss.setBaseImage(image)
@@ -52,6 +55,7 @@ def selective_search(image):
     ss_results = ss.process()
     return ss_results
 
+#?
 def selective_search_all():
     for i in tqdm(range(1, 666), desc="Processing Images", unit="image"):
         # Load the image
@@ -95,7 +99,14 @@ def selective_search_all():
         with open(f'Potholes/annotated-images/img-{i}_ss.json', "w") as file:
             json.dump(results_list, file)
 
-def load_test_and_train(path):
+
+# Loads the train and test splist from the "splits" json file
+def load_test_and_train():
+    # Loads the train and test splist from the "splits" json file
+
+    #path to file
+    path = "Potholes/splits.json"
+    
     # Load the JSON file
     with open(path, 'r') as f:
         data = json.load(f)
@@ -106,7 +117,10 @@ def load_test_and_train(path):
 
     return train_data, test_data
 
+#reads xml file
 def read_content(xml_file: str):
+    # Reads content of given xml file
+    # Return image name and list of bounding box values
 
     tree = ET.parse(xml_file)
     root = tree.getroot()
@@ -129,6 +143,7 @@ def read_content(xml_file: str):
 
     return filename, list_with_all_boxes
 
+# plots image from data with its ground truth bounding boxes
 def plot_image_with_boxes(image_path):
     """
     Plots an image with bounding boxes in xmin, ymin, xmax, ymax format.
@@ -155,6 +170,7 @@ def plot_image_with_boxes(image_path):
         # Add the rectangle to the plot
         ax.add_patch(rect)
 
+#opens and returns image given its name in string format
 def return_image(image_path):
     path = "Potholes/annotated-images/"
     name, boxes = read_content(path + image_path)
@@ -162,6 +178,7 @@ def return_image(image_path):
     # Load image
     return Image.open(path + name)
 
+#returns path to image
 def return_image_path(image_path):
     path = "Potholes/annotated-images/"
     name, boxes = read_content(path + image_path)
@@ -169,28 +186,27 @@ def return_image_path(image_path):
     # Load image
     return path + name
 
-def convert_bbox(x, y, width, height):
+#converts from [x,y,w,h] to [xmin, ymin, xmax, ymax]
+def wh_to_minmax(x, y, width, height):
+    # Converts the bounding box values, x, y, width to xmin, ymin, xmax, ymax
     xmin = x
     ymin = y
     xmax = x + width
     ymax = y + height
     return xmin, ymin, xmax, ymax
 
-def calculate_iou(box1, box2):
-    x1 = max(box1[0], box2[0])
-    y1 = max(box1[1], box2[1])
-    x2 = min(box1[2], box2[2])
-    y2 = min(box1[3], box2[3])
+#converts from [xmin, ymin, xmax, ymax] to [x,y,w,h] 
+def minmax_to_wh(xmin, ymin, xmax, ymax):
+    # Converts the bounding box values [xmin, ymin, xmax, ymax] to  ]x, y, width]
+    x = xmin
+    y = ymin
+    w = xmax - xmin
+    h = ymax - ymin
+    return x, y, w, h
 
-    intersection_area = max(0, x2 - x1) * max(0, y2 - y1)
-    
-    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
-    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
-
-    iou = intersection_area / float(box1_area + box2_area - intersection_area)
-    return iou
-
+# compares proposal BB with all GT BB from image. Also sorts them using k1 and k2
 def compare_bb(image_path, bb, k1, k2):
+    # NOT USED 
     proposals = []
     background = []
 
@@ -245,4 +261,81 @@ def compare_bb(image_path, bb, k1, k2):
 
 
     return proposals, background
+
+
+
+## ____________ Dataset creation _______________________
+# functioner der bruges til at danne vores dataset
+
+#reads json file from path "Potholes/annotated-images/"
+def read_json(file_path):
+    # Reads the json file.
+    # Takes only name of file as input
+
+    path = "Potholes/annotated-images/"
+    with open(path + file_path, 'r') as file:
+        return json.load(file)
+
+#Splist list into three lists with different 'class' values
+def split_json_by_class(json):
+    #takes output from "read_json()" as input
+
+    class_0 = [entry for entry in json if entry.get("class") == 0]
+    class_1 = [entry for entry in json if entry.get("class") == 1]
+    class_none = [entry for entry in json if entry.get("class") is None]
+    
+    return class_0, class_1, class_none
+
+#Merges two lists
+def merge_classes(list_0, list_1):
+    # mearges two lists
+    return list_0 + list_1
+
+#finds id numbers for training data.
+def get_id(string):
+    # gets id's of training or test data
+    # Input is output from "load_test_and_train()" function
+
+    def extract_number(string):
+        match = re.search(r'\d+', string)
+        return int(match.group()) if match else None
+    
+    id = [extract_number(s) for s in train]
+    return id
+
+#reads all "img-1_ss.json" typed files in the training set and returns
+# three lists  with different 'class' values
+def create_1_0_none_splits():
+    sets = []
+    class_0 = []
+    class_1 = []
+    class_none = []
+    train, _ = load_test_and_train("Potholes/splits.json")
+
+    Ids = get_id(train)
+    
+    for id in Ids:
+        name = "img-"+str(id)+"_ss.json"
+        json = read_json(name)
+        cl_0, cl_1, cl_none = split_json_by_class(json)
+        class_0 = merge_classes(class_0,cl_0)
+        class_1 = merge_classes(class_1,cl_1)
+        class_none = merge_classes(class_none,cl_none)
+
+    return class_0, class_1, class_none
+
+# given output from "create_1_0_none_splits()" function creates a list of size batch_size 
+# with p1 % background proposals and 1-p1 pothole proposals.
+def create_batch(class_0, class_1, batch_size, p1):
+
+    num_background = int(batch_size * (p1))
+    num_class = batch_size - num_background
+
+    background = random.sample(class_0, min(num_background, len(class_0)))
+    class1 = random.sample(class_1, min(num_class, len(class_1)))
+
+    return merge_classes(background,class1)
+
+
+
 
