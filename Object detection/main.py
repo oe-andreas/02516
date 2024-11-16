d17e0fbdca63586d1a4fdf4a1472547119bf0409
oe-andreas <andreas.oestergaard20@gmail.com>
2024-11-16 22:26:04 +0100

    train set for train set

--- Object detection/main.py
+++ Object detection/main.py
@@ -30,7 +30,7 @@ t = time()
 
 
 input_size = get_input_size(model_name)
-train_loader = Dataloader(train="test", dim=[input_size, input_size], batch_size=64)
+train_loader = Dataloader(train="train", dim=[input_size, input_size], batch_size=64)
 val_loader = Dataloader(train="val", dim=[input_size, input_size], batch_size=64)
 
 print(f"Initialized Data Loader in {time() - t:.2}s")
